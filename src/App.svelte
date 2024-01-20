e3246e2dd0ff7f23d3c14e86e4746e83e55e4e26
Nathan <nathanrath@live.co.uk>
2024-01-20 14:52:02 +0000

    tests

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,60 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { OpusDecoder } from "opus-decoder";
+
+  const CHANNELS = 2;
+
+  const opusDecoder = new OpusDecoder({
+    channels: CHANNELS,
+  });
+
+  let audioContext: AudioContext | undefined = undefined;
+  let nextTime = 0;
+
+  fetch("http://localhost:8081/test").then(async ({ body }) => {
+    const stream = body as ReadableStream<Uint8Array>;
+    const reader = stream.getReader();
+
+    while (true) {
+      const streamPacket = (await reader.read()).value;
+
+      await opusDecoder.ready;
+
+      try {
+        const { channelData, samplesDecoded, sampleRate } =
+          opusDecoder.decodeFrame(streamPacket);
+
+        if (!audioContext) {
+          audioContext = new AudioContext();
+
+          nextTime = 0;
+        }
+
+        const audioSource = audioContext.createBufferSource();
+        audioSource.connect(audioContext.destination);
+
+        const audioBuffer = audioContext.createBuffer(
+          channelData.length,
+          samplesDecoded,
+          sampleRate
+        );
+
+        for (const i in channelData) {
+          const data = channelData[i];
+          audioBuffer.copyToChannel(data, parseInt(i));
+        }
+
+        audioSource.buffer = audioBuffer;
+
+        nextTime += audioBuffer.duration;
+        audioSource?.start(nextTime);
+      } catch (err) {
+        continue;
+      }
+    }
+  });
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<main></main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
