9c9aae4ddc9900f0876704f1eb723eeb93469352
Nathan <nathanrath@live.co.uk>
2024-10-07 12:14:36 +0100

    configured vite for spa site generation

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -35,7 +35,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: "dist",
+			assets: "dist",
+			fallback: "index.html"
+		}),
 		alias: {
 			$shared: "../claudia-shared",
 
